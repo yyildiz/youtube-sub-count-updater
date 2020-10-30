6b537c65e815c4df369b1a4b59cea68947c23c3d
Yusuf Yildiz <yusuf_yildiz@student.uml.edu>
2020-10-30 01:28:29 -0400

    Initial commit for YouTube sub count tracker on Instagram Descrioption

---
+++ app.py
@@ -0,0 +1,50 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os 
+import helpers
+
+load_dotenv()
+insta_api = helpers.get_instagram_api()
+api_key = os.getenv('YT_DATA_API_KEY')
+channel_id = os.getenv('CHANNEL_ID')
+insta_user_id = os.getenv('INSTAUSERID')
+insta_email = os.getenv('INSTAEMAIL')
+
+
+def getSubscriberCount():
+    url = f'https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id={channel_id}&key={api_key}'
+    json_url = requests.get(url)
+    data = json.loads(json_url.text)
+    items = data.get('items', {})
+    return items[0].get('statistics', {}).get('subscriberCount')
+
+def updateDescription():
+    current_sub_count = getSubscriberCount()
+    SUB_COUNT_IDENTIFIER = 'Current YT Sub Count: '
+    user_info = insta_api.user_info(insta_user_id)
+    user = user_info.get('user', {})
+    
+    bio = user.get('biography').strip()
+    name = user.get('full_name')
+    url = user.get('external_url')
+    gender = 1
+    email = insta_email
+    new_bio = ''
+    has_changed = False
+    for line in bio.split('\n'):
+        if SUB_COUNT_IDENTIFIER in line:
+            count = line.split(SUB_COUNT_IDENTIFIER)[1]
+            if count != current_sub_count:
+                line = SUB_COUNT_IDENTIFIER + current_sub_count
+                has_changed = True
+        new_bio += line + '\n'
+    
+    if has_changed:
+        insta_api.edit_profile(biography=new_bio, first_name=name, external_url=url, gender=gender, phone_number=None, email=email)    
+    else:
+        print('no change!')
+
+updateDescription()
+# if __name__ == '__main__':
+#     updateDescription()
---
+++ helpers.py
@@ -0,0 +1,105 @@
+import json
+import codecs
+import datetime
+import os.path
+import logging
+import argparse
+import os
+from dotenv import load_dotenv
+load_dotenv()
+try:
+    from instagram_private_api import (
+        Client, ClientError, ClientLoginError,
+        ClientCookieExpiredError, ClientLoginRequiredError,
+        __version__ as client_version)
+except ImportError:
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    from instagram_private_api import (
+        Client, ClientError, ClientLoginError,
+        ClientCookieExpiredError, ClientLoginRequiredError,
+        __version__ as client_version)
+
+
+def to_json(python_object):
+    if isinstance(python_object, bytes):
+        return {'__class__': 'bytes',
+                '__value__': codecs.encode(python_object, 'base64').decode()}
+    raise TypeError(repr(python_object) + ' is not JSON serializable')
+
+
+def from_json(json_object):
+    if '__class__' in json_object and json_object['__class__'] == 'bytes':
+        return codecs.decode(json_object['__value__'].encode(), 'base64')
+    return json_object
+
+
+def onlogin_callback(api, new_settings_file):
+    cache_settings = api.settings
+    with open(new_settings_file, 'w') as outfile:
+        json.dump(cache_settings, outfile, default=to_json)
+        print('SAVED: {0!s}'.format(new_settings_file))
+
+
+def get_instagram_api():
+    logging.basicConfig()
+    logger = logging.getLogger('instagram_private_api')
+    logger.setLevel(logging.WARNING)
+    
+    print('Client version: {0!s}'.format(client_version))
+
+    device_id = None
+    try:
+        settings_file_path = 'config.json'
+        settings_file = settings_file_path
+        username = os.getenv('INSTAUSER')
+        password = os.getenv('INSTAPASS')
+        if not os.path.isfile(settings_file):
+            # settings file does not exist
+            print('Unable to find file: {0!s}'.format(settings_file))
+
+            # login new
+            api = Client(
+                username, password,
+                on_login=lambda x: onlogin_callback(x, settings_file_path))
+        else:
+            with open(settings_file) as file_data:
+                cached_settings = json.load(file_data, object_hook=from_json)
+            print('Reusing settings: {0!s}'.format(settings_file))
+
+            device_id = cached_settings.get('device_id')
+            # reuse auth settings
+            api = Client(
+                username, password,
+                settings=cached_settings)
+
+    except (ClientCookieExpiredError, ClientLoginRequiredError) as e:
+        print('ClientCookieExpiredError/ClientLoginRequiredError: {0!s}'.format(e))
+
+        # Login expired
+        # Do relogin but use default ua, keys and such
+        api = Client(
+            username, password,
+            device_id=device_id,
+            on_login=lambda x: onlogin_callback(x, settings_file_path))
+
+    except ClientLoginError as e:
+        print('ClientLoginError {0!s}'.format(e))
+        exit(9)
+    except ClientError as e:
+        print('ClientError {0!s} (Code: {1:d}, Response: {2!s})'.format(e.msg, e.code, e.error_response))
+        exit(9)
+    except Exception as e:
+        print('Unexpected Exception: {0!s}'.format(e))
+        exit(99)
+
+    # Show when login expires
+    cookie_expiry = api.cookie_jar.auth_expires
+    print('Cookie Expiry: {0!s}'.format(datetime.datetime.fromtimestamp(cookie_expiry).strftime('%Y-%m-%dT%H:%M:%SZ')))
+
+    # Call the api
+    results = api.user_feed('2958144170')
+    assert len(results.get('items', [])) > 0
+
+    print('All ok')
+    return api
